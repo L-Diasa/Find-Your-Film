c2a270ca48676464baf835a75e439a16abf4ac69
L-Diasa <likadiasa@gmail.com>
2022-08-13 11:06:00 +0400

    added date sorter to the watchlist page

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,25 +1,48 @@
-import React, { useContext }from "react"
+import React, { useContext, useState, useEffect }from "react"
 
 import Header from '../components/Header'
 import Movie from '../components/Movie'
 import { Context } from "../Context"
 
-export default function Watchlist() {
-    const { watchlistItems } = useContext(Context)
+import ascendingIcon from "../images/ascendingIcon.svg"
+import descendingIcon from "../images/descendingIcon.svg"
 
-    const imageElements = watchlistItems.map(movieId => {
-        return (<Movie 
-                    key={movieId} 
-                    id={movieId} 
-                />)
-    })
+export default function Watchlist() {
+    const { watchlistItems, lastRemovedId } = useContext(Context)
+
+    const [sotredArray, setSortedArray] = useState(null)
+    const [currSortOrder, setCurrSortOrder] = useState("asc")
+
+    useEffect(() => {
+        setSortedArray(
+            watchlistItems.map(movieId => {
+                return (<Movie 
+                            key={movieId} 
+                            id={movieId} 
+                        />)
+                }
+            )
+        )
+    }, [])
+
+    useEffect(() => {
+        setSortedArray(prevArray => prevArray.filter(movieId => movieId.key != lastRemovedId))
+    }, [lastRemovedId])
+
+    function toggleDate() {
+        setSortedArray(prevArray => [...prevArray].reverse())
+        setCurrSortOrder(prev => prev === "asc" ? "desc" : "asc")
+    }
 
     return (
         <>
             <Header goTo="/Finder" pageTitle="My Watchlist" />
             <main>
-                {imageElements.length ?
-                    imageElements :
+                <div className="watchlist-sorter selected-finder-link" onClick={toggleDate}>
+                    Date Added<img src={currSortOrder === "asc" ? ascendingIcon : descendingIcon} alt="" />
+                </div>
+                {sotredArray ?
+                    sotredArray :
                     <div className="smthsUp">
                         Your watchlist is looking a little empty...
                     </div>

02658d23cb1e58da51816e87a8a9ce3ce55b4814
L-Diasa <likadiasa@gmail.com>
2022-08-13 14:07:54 +0400

    updated the watchlist page

--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect }from "react"
+import { Link } from "react-router-dom"
 
 import {Header, HeaderLinkSearch } from '../components/Header'
 import Movie from '../components/Movie'
@@ -6,11 +7,12 @@ import { Context } from "../Context"
 
 import ascendingIcon from "../images/ascendingIcon.svg"
 import descendingIcon from "../images/descendingIcon.svg"
+import addIcon from "../images/addIcon.svg"
 
 export default function Watchlist() {
     const { watchlistItems, lastRemovedId } = useContext(Context)
 
-    const [sotredArray, setSortedArray] = useState(null)
+    const [sotredArray, setSortedArray] = useState([])
     const [currSortOrder, setCurrSortOrder] = useState("asc")
 
     useEffect(() => {
@@ -37,14 +39,21 @@ export default function Watchlist() {
     return (
         <>
             <Header pageTitle="My Watchlist" link={<HeaderLinkSearch />} />
-            <main>
-                <div className="watchlist-sorter selected-finder-link" onClick={toggleDate}>
-                    Date Added<img src={currSortOrder === "asc" ? ascendingIcon : descendingIcon} alt="" />
-                </div>
-                {sotredArray ?
-                    sotredArray :
+            <main>            
+                {sotredArray.length ?
+                    <>
+                    <div className="watchlist-sorter selected-finder-link" onClick={toggleDate}>
+                        Date Added<img src={currSortOrder === "asc" ? ascendingIcon : descendingIcon} alt="" />
+                    </div>
+                    {sotredArray}
+                    </>
+                    :
                     <div className="smthsUp">
                         Your watchlist is looking a little empty...
+                        <Link className="lets-add" to="/finder">
+                            <img src={addIcon} alt="" />
+                            Let’s add some movies!
+                        </Link>
                     </div>
                 }
             </main>

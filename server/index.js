dffa76691b055e3be5eb192581559a79a47314ee
L-Diasa <likadiasa@gmail.com>
2022-08-26 22:22:32 +0400

    deploying to heroku

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require('express')
-const cors = require('cors')
+const path = require('path')
 const axios = require('axios')
 require('dotenv').config()
+
 const PORT = process.env.PORT || 3001
 
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
 
 app.get("/genrelist", (req, res) => {
   const options = {
@@ -75,6 +72,17 @@ app.get("/searchwithgenre", (req, res) => {
       res.json(response.data)
   })
 })
+
+
+app.get('/api', (req, res) => {
+    res.json({message: "yoo"})
+  });
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`)

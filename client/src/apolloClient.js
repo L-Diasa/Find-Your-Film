02df3b4f712fb9fcfa45e9ae9b68971d332efd0a
L-Diasa <likadiasa@gmail.com>
2022-09-05 23:54:28 +0400

    fixed a network error

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-    uri: `http://localhost:${process.env.AUTH_PORT}/`
+    uri: `http://localhost:${process.env.REACT_APP_AUTH_PORT}/`
 })
 
 const authLink = setContext((_, { headers }) => {

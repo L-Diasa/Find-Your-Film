0270007ebad528510212e6e761df654c6a43bb9d
L-Diasa <likadiasa@gmail.com>
2022-09-08 21:57:32 +0400

    updated app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import {Routes, Route, Navigate} from "react-router-dom"
+import React, { useContext } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom"
+import { AuthContext } from './context/AuthContext'
 
 import Details from "./pages/Details"
 import Finder from "./pages/Finder"
 import Watchlist from "./pages/Watchlist"
+import Auth from "./pages/Auth"
 
 export default function App() {
+  const { user } = useContext(AuthContext)
+
   return (
     <>
       <Routes>
-        <Route exact path="/Watchlist" element={<Watchlist />}/>
-        <Route exact path="/Details" element={<Details/>}/>
-        <Route path="/Finder/*" element={<Finder />}/>
-        <Route path="/*" element={<Navigate to="/Finder" />}/>
+        {user !== null ? 
+        <>
+          <Route exact path="/Watchlist" element={<Watchlist />}/>
+          <Route exact path="/Details" element={<Details/>}/>
+          <Route path="/Finder/*" element={<Finder/>}/>
+          <Route path="/*" element={<Navigate to="/Finder"/>}/>
+        </>
+        : 
+        <>
+          <Route path="/*" element={<Navigate to="/Auth"/>}/>
+          <Route path="/Auth" element={<Auth/>}/>
+        </>
+        }
       </Routes>
     </>
   )
